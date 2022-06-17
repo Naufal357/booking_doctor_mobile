9fc47881001feae2c4416fd718bf630a9fff3119
salmanurrahma <99798368+salmanurrahma@users.noreply.github.com>
2022-06-17 19:14:39 +0700

    Update admin_category_add_binding.dart

--- lib/app/modules/admin-category-add/bindings/admin_category_add_binding.dart
+++ lib/app/modules/admin-category-add/bindings/admin_category_add_binding.dart
@@ -7,6 +7,7 @@ class AdminCategoryAddBinding extends Bindings {
   void dependencies() {
     Get.lazyPut<AdminCategoryAddController>(
       () => AdminCategoryAddController(),
+      
     );
   }
 }
